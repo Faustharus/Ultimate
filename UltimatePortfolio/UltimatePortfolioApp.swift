60f12d59c38bdbf5c39b62fa270bbac8ec612876
Faustharus <chailloleau-damien@outlook.fr>
2024-08-17 11:23:00 +0200

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Damien Chailloleau on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
