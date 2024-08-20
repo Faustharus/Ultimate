8ee96e28cee621b4d740552050e435ddf4914e6e
Faustharus <chailloleau-damien@outlook.fr>
2024-08-20 12:56:08 +0200

    CoreData first functionnality inserted

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -51,4 +51,37 @@ class DataController: ObservableObject {
         
         try? viewContext.save()
     }
+    
+    func save() {
+        if container.viewContext.hasChanges {
+            try? container.viewContext.save()
+        }
+    }
+    
+    func delete(_ object: NSManagedObject) {
+        objectWillChange.send()
+        container.viewContext.delete(object)
+        save()
+    }
+    
+    private func delete(_ fetchRequest: NSFetchRequest<NSFetchRequestResult>) {
+        let batchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        batchDeleteRequest.resultType = .resultTypeObjectIDs
+        
+        if let delete = try? container.viewContext.execute(batchDeleteRequest) as? NSBatchDeleteResult {
+            let changes = [NSDeletedObjectsKey: delete.result as? [NSManagedObjectID] ?? []]
+            NSManagedObjectContext.mergeChanges(fromRemoteContextSave: changes, into: [container.viewContext])
+        }
+    }
+    
+    func deleteAll() {
+        let request1: NSFetchRequest<NSFetchRequestResult> = Tag.fetchRequest()
+        delete(request1)
+        
+        let request2: NSFetchRequest<NSFetchRequestResult> = Issue.fetchRequest()
+        delete(request2)
+        
+        save()
+    }
+    
 }
