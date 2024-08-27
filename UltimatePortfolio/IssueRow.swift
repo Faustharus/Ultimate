7dfa1ba53d37f4a962e6101dbdb426bf9d5b8de7
Faustharus <chailloleau-damien@outlook.fr>
2024-08-27 18:35:11 +0200

    Adding IssueRow View and ContentView

---
+++ UltimatePortfolio/IssueRow.swift
@@ -0,0 +1,52 @@
+//
+//  IssueRow.swift
+//  UltimatePortfolio
+//
+//  Created by Damien Chailloleau on 27/08/2024.
+//
+
+import SwiftUI
+
+struct IssueRow: View {
+    
+    @EnvironmentObject var dataController: DataController
+    @ObservedObject var issue: Issue
+    
+    var body: some View {
+        NavigationLink(value: issue) {
+            HStack {
+                Image(systemName: "exclamationmark.circle")
+                    .imageScale(.large)
+                    .opacity(issue.priority == 2 ? 1 : 0)
+                
+                VStack(alignment: .leading) {
+                    Text(issue.issueTitle)
+                        .font(.headline)
+                        .lineLimit(1)
+                    
+                    Text("No tags")
+                        .foregroundStyle(.secondary)
+                        .lineLimit(1)
+                }
+                
+                Spacer()
+                
+                VStack(alignment: .trailing) {
+                    Text(issue.issueCreationDate.formatted(date: .numeric, time: .omitted))
+                    
+                    if issue.completed {
+                        Text("Closed")
+                            .textInputAutocapitalization(.words)
+                            .font(.body.smallCaps())
+                    }
+                }
+                .foregroundStyle(.secondary)
+            }
+        }
+    }
+}
+
+#Preview {
+    IssueRow(issue: .example)
+        .environmentObject(DataController.preview)
+}
