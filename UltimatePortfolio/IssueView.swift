bb67d0d936924cb830776efb4d93e6e3ba6f4427
Faustharus <chailloleau-damien@outlook.fr>
2024-09-02 11:20:33 +0200

    Adding of NoIssueView and IssueView

---
+++ UltimatePortfolio/IssueView.swift
@@ -0,0 +1,18 @@
+//
+//  IssueView.swift
+//  UltimatePortfolio
+//
+//  Created by Damien Chailloleau on 02/09/2024.
+//
+
+import SwiftUI
+
+struct IssueView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    IssueView()
+}
---
+++ UltimatePortfolio/NoIssueView.swift
@@ -0,0 +1,29 @@
+//
+//  NoIssueView.swift
+//  UltimatePortfolio
+//
+//  Created by Damien Chailloleau on 02/09/2024.
+//
+
+import SwiftUI
+
+struct NoIssueView: View {
+    @EnvironmentObject var dataController: DataController
+    
+    var body: some View {
+        VStack {
+            Text("No Issue Selected !")
+                .font(.title)
+                .foregroundStyle(.secondary)
+            
+            Button("New Issue") {
+                // MARK: More code later
+            }
+        }
+    }
+}
+
+#Preview {
+    NoIssueView()
+        .environmentObject(DataController.preview)
+}
